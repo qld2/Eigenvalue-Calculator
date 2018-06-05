04858aa51ce6209901e2733fc498dd206550b466
qld2 <qld2@illinois.edu>
2018-06-05 12:03:22 -0500

    Began overloading functions

--- Eigenvalue-Calculator/Polynomial.cpp
+++ Eigenvalue-Calculator/Polynomial.cpp
@@ -28,26 +28,50 @@ private:
 		int* newCoeffs = new int[newTermCount];
 		int* newPowers = new int[newTermCount];
 
-		int k = 0;
-		for (int i = 0; i < termCount; i++) {
-			for (int j = i + 1; j < termCount; j++) {
-				if (powers[j] == powers[i]) {
-					newCoeffs[k] += coeffs[j];
+		int k = 1;
+		newPowers[0] = powers[0];
+		newCoeffs[0] = coeffs[0];
+		for (int i = 1; i < termCount; i++) {
+
+			bool found = false;
+
+			for (int j = 0; j < k; j++) {
+				if (powers[i] == newPowers[j]) {
+					newCoeffs[j] += coeffs[i];
+					found = true;
 				}
 			}
+
+			if (!found) {
+				newPowers[k] = powers[k];
+				newCoeffs[k] = coeffs[k];
+				k++;
+			}
 		}
-
-
-
 	}
 
 public:
+	Polynomial()
+	{
+		termCount = 1;
+		coeffs = new int[1];
+		powers = new int[1];
+
+		coeffs[0] = 0;
+		powers[0] = 1;
+	}
+
 	Polynomial(int terms, int* c, int* p) {
 		termCount = terms;
 		coeffs = c;
 		powers = p;
 
 		//combineLikeTerms();
+	}
+	
+	~Polynomial()
+	{
+		delete coeffs, powers;
 	}
 
 
@@ -107,6 +131,26 @@ public:
 			/ generateDerivative().evalAtPoint(start_point));
 	}
 
+	/*
+	Polynomial& operator + (const Polynomial& p) {
+		const int* pCoeffs = p.getCoeffs();
+		const int* pPowers = p.getPowers();
+
+		int* rCoeffs = new int[termCount];
+		int* rPowers = new int[termCount];
+
+		for (int i = 0; i < termCount; i++) {
+			for (int j = 0; j < termCount; j++) {
+				rCoeffs[k] = coeffs[i] * pCoeffs[j];
+				rPowers[k] = powers[i] + pPowers[j];
+				cout << rCoeffs[k] << " " << rPowers[k] << endl;
+			}
+		}
+
+		Polynomial* p2 = new Polynomial(termCount, rCoeffs, rPowers);
+		return (*p2);
+	}
+	*/
 
 	Polynomial& operator * (const Polynomial& p) {
 		const int* pCoeffs = p.getCoeffs();
@@ -129,4 +173,33 @@ public:
 		return (*p2);
 	}
 
+	Polynomial& operator * (double scalar) {
+
+		int* rCoeffs = new int[termCount];
+		int* rPowers = new int[termCount];
+
+		for (int i = 0; i < termCount; i++) {
+			rPowers[i] = powers[i];
+			rCoeffs[i] = coeffs[i] * scalar;
+		}
+
+		Polynomial* p2 = new Polynomial(termCount, rCoeffs, rPowers);
+		return (*p2);
+	}
+
+	friend ostream& operator << (ostream &s, Polynomial& p) {
+		for (int i = 0; i < termCount; i++) {
+			s << coeffs[i] << " x^" << powers[i] << " ";
+
+			if (i != termCount - 1) {
+				s << "+ ";
+			}
+		}
+
+		s << endl;
+
+		return s;
+	}
 };
+
+
