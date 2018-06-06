7d543ae8fd2abfd6db9ec7dd6ce4f96bcb3b7c6f
qld2 <qld2@illinois.edu>
2018-06-06 17:54:22 -0500

    Made correct header file for Polynomial

--- Eigenvalue-Calculator/Polynomial.cpp
+++ Eigenvalue-Calculator/Polynomial.cpp
@@ -1,3 +1,4 @@
+#pragma once
 #include "stdafx.h"
 #include "Polynomial.h"
 #include <iostream>
@@ -5,24 +6,18 @@
 
 using namespace std;
 
-class Polynomial {
-private:
-	int termCount;
-	int* coeffs, *powers;
+void Polynomial::combineLikeTerms() {
 
+	int repeatCount = 0;
 
-	void combineLikeTerms() {
-
-		int repeatCount = 0;
-
-		for (int i = 0; i < termCount; i++) {
-			for (int j = i + 1; j < termCount; j++) {
-				if (powers[j] == powers[i]) {
-					repeatCount++;
-					break;
-				}
+	for (int i = 0; i < termCount; i++) {
+		for (int j = i + 1; j < termCount; j++) {
+			if (powers[j] == powers[i]) {
+				repeatCount++;
+				break;
 			}
 		}
+	}
 
 		int newTermCount = termCount - repeatCount;
 		int* newCoeffs = new int[newTermCount];
@@ -50,86 +45,83 @@ private:
 		}
 	}
 
-public:
-	Polynomial()
-	{
+Polynomial::Polynomial() {
 		termCount = 1;
 		coeffs = new int[1];
 		powers = new int[1];
 
 		coeffs[0] = 0;
 		powers[0] = 1;
-	}
+}
 
-	Polynomial(int terms, int* c, int* p) {
-		termCount = terms;
-		coeffs = c;
-		powers = p;
+Polynomial::Polynomial(int terms, int* c, int* p) {
+	termCount = terms;
+	coeffs = c;
+	powers = p;
 
-		//combineLikeTerms();
-	}
+	//combineLikeTerms();
+}
 	
-	~Polynomial()
-	{
-		delete coeffs, powers;
+Polynomial::~Polynomial() {
+	delete coeffs, powers;
+}
+
+
+const int* const Polynomial::getCoeffs() const {
+	return coeffs;
+}
+
+const int* const Polynomial::getPowers() const {
+	return powers;
+}
+
+const int Polynomial::getTermCount(){
+	return termCount;
+}
+
+
+double Polynomial::evalAtPoint(double point) {
+	double sum = 0;
+	for (int i = 0; i < termCount; i++) {
+		sum += coeffs[i] * pow(point, powers[i]);
+	}
+	return sum;
+}
+
+
+Polynomial Polynomial::generateDerivative() {
+	int* c = new int[termCount];
+	int* p = new int[termCount];
+
+	for (int i = 0; i < termCount; i++) {
+		c[i] = coeffs[i] * powers[i];
+		p[i] = powers[i] - 1;
 	}
 
-
-	const int* const getCoeffs() const {
-		return coeffs;
-	}
-
-	const int* const getPowers() const {
-		return powers;
-	}
-
-	const int const getTermCount() const {
-		return termCount;
-	}
+	return Polynomial(termCount, c, p);
+}
 
 
-	double evalAtPoint(double point) {
-		double sum = 0;
-		for (int i = 0; i < termCount; i++) {
-			sum += coeffs[i] * pow(point, powers[i]);
-		}
-		return sum;
-	}
+double Polynomial::findRoot(double start_point) {
 
+	static int iterations = 100;
 
-	Polynomial generateDerivative() {
-		int* c = new int[termCount];
-		int* p = new int[termCount];
+	if (iterations == 1) {
+		iterations = 100;
+		double approx = start_point - ((double)evalAtPoint(start_point))
+			/ generateDerivative().evalAtPoint(start_point);
 
-		for (int i = 0; i < termCount; i++) {
-			c[i] = coeffs[i] * powers[i];
-			p[i] = powers[i] - 1;
-		}
-
-		return Polynomial(termCount, c, p);
-	}
-
-
-	double findRoot(double start_point) {
-
-		static int iterations = 100;
-
-		if (iterations == 1) {
-			iterations = 100;
-			int approx = start_point - ((double) evalAtPoint(start_point))
-				/ generateDerivative().evalAtPoint(start_point);
-
-			if (abs(approx) < 1e-7) {
-				approx = 0;
-			}
-
-			return approx;
+		if (abs(approx) < 1e-7) {
+			approx = 0;
 		}
 
-		iterations--;
-		return findRoot(start_point - ((double) evalAtPoint(start_point))
-			/ generateDerivative().evalAtPoint(start_point));
+		return approx;
 	}
+
+	iterations--;
+	return findRoot(start_point - ((double)evalAtPoint(start_point))
+		/ generateDerivative().evalAtPoint(start_point));
+}
 
 	/*
 	Polynomial& operator + (const Polynomial& p) {
@@ -150,7 +142,7 @@ public:
 		Polynomial* p2 = new Polynomial(termCount, rCoeffs, rPowers);
 		return (*p2);
 	}
-	*/
+	
 
 	Polynomial& operator * (const Polynomial& p) {
 		const int* pCoeffs = p.getCoeffs();
@@ -186,7 +178,7 @@ public:
 		Polynomial* p2 = new Polynomial(termCount, rCoeffs, rPowers);
 		return (*p2);
 	}
-
+*/
 	friend ostream& operator << (ostream &s, Polynomial& p) {
 		for (int i = 0; i < termCount; i++) {
 			s << coeffs[i] << " x^" << powers[i] << " ";
@@ -200,6 +192,6 @@ public:
 
 		return s;
 	}
-};
+	
 
 
--- Eigenvalue-Calculator/Polynomial.h
+++ Eigenvalue-Calculator/Polynomial.h
@@ -1,8 +1,24 @@
 #pragma once
-class Polynomial
-{
+
+class Polynomial{
+private:
+	int termCount;
+	int * coeffs, * powers;
+	
+	void combineLikeTerms();
 public:
 	Polynomial();
 	~Polynomial();
+	Polynomial(int terms, int* c, int* p);
+	
+	const int* const getCoeffs() const;
+	const int* const getPowers() const;
+	const int getTermCount();
+
+	double evalAtPoint(double point);
+	Polynomial generateDerivative();
+	double findRoot(double start_point);
+
+	friend ostream& operator << (ostream &s, Polynomial& p);
 };
 
